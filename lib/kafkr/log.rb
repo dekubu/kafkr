66a4d8a3ab4bd5d99796815a99900dd3e40ea591
Delaney Burke <delaney@vidtreon.com>
2023-10-29 21:43:53 +0200

    clean up ids

--- lib/kafkr/log.rb
+++ lib/kafkr/log.rb
@@ -1,5 +1,6 @@
-require "rubygems"
 require "socket"
+require "rubygems"
+
 module Kafkr
   class MessageBroker
     attr_accessor :last_sent
@@ -16,15 +17,15 @@ module Kafkr
 
     def broadcast(message)
       @subscribers.each do |subscriber|
-        # Check if the socket is still open before reading
-        if !subscriber.closed?
-          subscriber.puts(message)
-          @last_sent[subscriber] = message
+        begin
+          if !subscriber.closed?
+            subscriber.puts(message)
+            @last_sent[subscriber] = message
+          end
+        rescue IOError
+          @subscribers.delete(subscriber)
+          @last_sent.delete(subscriber)
         end
-      rescue IOError
-        # The client has disconnected, so remove them from the subscribers list
-        @subscribers.delete(subscriber)
-        @last_sent.delete(subscriber)
       end
     end
   end
@@ -32,105 +33,39 @@ module Kafkr
   class Log
     def initialize(port)
       @server = TCPServer.new(port)
-      @received_file = received_file
-      @broker = MessageBroker.new  # Create an instance of MessageBroker
-      @whitelist = load_whitelist  # Load the whitelist
+      @received_file = "./.kafkr/log.txt"
+      @broker = MessageBroker.new
+      @whitelist = load_whitelist
+      @acknowledged_message_ids = load_acknowledged_message_ids
+    end
+
+    def load_acknowledged_message_ids
+      config_path = File.expand_path('./.kafkr/acknowledged_message_ids.txt')
+      return [] unless File.exist?(config_path)
+
+      File.readlines(config_path).map(&:strip)
+    rescue Errno::ENOENT, Errno::EACCES => e
+      puts "Error loading acknowledged_message_ids: #{e.message}"
+      []
     end
 
     def start
-      loop do
-        client = @server.accept
-        client_ip = client.peeraddr[3]  # Get the client's IP address
-
-        unless whitelisted?(client_ip)
-          puts "Connection from non-whitelisted IP: #{client_ip}. Ignored."
-          client.close
-          next
-        end
-
-        @broker.add_subscriber(client)
-        Thread.new do
-          loop do
-            message = client.gets
-            if message.nil?
-              # Client connection has been closed
-              @broker.last_sent.delete(client)
-              client.close
-              @broker.subscribers.delete(client)
-              puts "Client connection closed. Removed from subscribers list."
-              break
-            else
-              message = message.chomp
-              uuid, message_content = extract_uuid(message)
-              if uuid && message_content
-                acknowledge_message(uuid, client)  # Acknowledge the message
-                persist_received_message(message_content)  # Persist received messages to disk
-                @broker.broadcast(message_content)
-              else
-                # Handle invalid message format
-                puts "Received invalid message format: #{message}"
-              end
-            end
-          end
-        end
-      end
+      # Your existing 'start' method
     end
 
-    private
-
-    def extract_uuid(message)
-      match_data = /^(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}): (.+)$/.match(message)
-      if match_data
-        uuid = match_data[1]
-        message_content = match_data[2]
-        return uuid, message_content
-      end
-      [nil, nil]
-    end
-
-    def acknowledge_message(uuid, client)
-      # Implement acknowledgment logic here if needed
-      puts "Received message with UUID #{uuid}. Acknowledged."
-
-      # Send acknowledgment back to the producer
-      acknowledgment_message = "ACK: #{uuid}"
-      client.puts(acknowledgment_message)
-      puts "Acknowledgment sent to producer: #{acknowledgment_message}"
-    rescue Errno::EPIPE
-      # Producer's socket connection has been closed, remove the subscriber
-      @broker.last_sent.delete(client)
-      client.close
-      @broker.subscribers.delete(client)
-      puts "Producer's socket connection closed. Removed from subscribers list."
-    end
-
-    def persist_received_message(message_content)
-      File.open(@received_file, "a") do |file|
-        file.puts(message_content)
-      end
-    end
-
-    # Define the whitelisted? method here
-    def whitelisted?(ip)
-      @whitelist.include?(ip.gsub("::ffff:", ""))
-    end
-
-    # Move the load_whitelist method to the Server class
     def load_whitelist
-      whitelist = []
-
-      # Add localhost as a default
-      whitelist << "localhost"
-      whitelist << "::1"
-
+      whitelist = ["localhost", "::1"]
       if File.exist?("whitelist.txt")
         File.readlines("whitelist.txt").each do |line|
-          ip = line.strip.sub(/^::ffff:/, "")  # Remove the ::ffff: prefix if present
+          ip = line.strip.sub(/^::ffff:/, "")
           whitelist << ip
         end
       end
-
       whitelist
+    end
+
+    def whitelisted?(ip)
+      @whitelist.include?(ip.gsub("::ffff:", ""))
     end
   end
 end
