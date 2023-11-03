2cb6d9806834d35789bc126fb52df6ad2ab0a387
Delaney Burke <delaney@vidtreon.com>
2023-11-03 07:21:28 +0200

    fix the error Decryption failed: wrong final block length

--- lib/kafkr/encryptor.rb
+++ lib/kafkr/encryptor.rb
@@ -3,13 +3,13 @@ require "base64"
 
 module Kafkr
   class Encryptor
-    ALGORITHM = "AES-256-CBC"
-
+    ALGORITHM = 'AES-256-CBC'
+  
     def initialize
-      @key = Base64.decode64("2wZ85yxQe0lmiQ5nsqdmPWoGB0W6HZW8S/UXVTLQ6WY=")
+      @key = Base64.decode64("2wZ85yxQe0lmiQ5nsqdmPWoGB0W6HZW8S/UXVTLQ6WY=") 
       @cipher = OpenSSL::Cipher.new(ALGORITHM)
     end
-
+  
     def encrypt(data)
       cipher = OpenSSL::Cipher.new(ALGORITHM)
       cipher.encrypt
@@ -26,25 +26,12 @@ module Kafkr
       cipher.key = @key
 
       decoded_data = Base64.decode64(encrypted_data)
-      cipher.iv = decoded_data[0..15] # Exact IV that was used for encryption
-
-      encrypted_message = decoded_data[16..-1]
-
-      cipher.update(encrypted_message) + cipher.final
-    end
-
-    def decrypt(encrypted_data)
-      cipher = OpenSSL::Cipher.new(ALGORITHM)
-      cipher.decrypt
-      cipher.key = @key
-
-      decoded_data = Base64.decode64(encrypted_data)
 
       # The IV is exactly the first 16 bytes of the decoded data
       cipher.iv = decoded_data[0, cipher.iv_len]
 
       # The encrypted message is everything after the IV
-      encrypted_message = decoded_data[cipher.iv_len..-1]
+      encrypted_message = decoded_data[cipher.iv_len..]
 
       # Decrypt and return the plaintext message
       cipher.update(encrypted_message) + cipher.final
