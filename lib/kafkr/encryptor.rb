8ee3007253f7dae9f747596cd77eef1df3b979c6
Delaney Burke <delaney@vidtreon.com>
2023-11-03 06:58:52 +0200

    fix the enxrptyor call

--- lib/kafkr/encryptor.rb
+++ lib/kafkr/encryptor.rb
@@ -1,27 +1,34 @@
+require 'openssl'
+require 'base64'
+
 module Kafkr
-    class Encryptor
-      ALGORITHM = 'AES-256-CBC'
+  class Encryptor
+    ALGORITHM = 'AES-256-CBC'
   
-      def initialize
-        @key = ENV['KAFKR_KEY']
-        @cipher = OpenSSL::Cipher.new(ALGORITHM)
-      end
-  
-      def encrypt(data)
-        @cipher.encrypt data
-        @cipher.key = @key
-        iv = @cipher.random_iv
-  
-        encrypted_data = @cipher.update(data) + @cipher.final
-        "#{iv.unpack1('m')}--#{encrypted_data.unpack1('m')}"
-      end
-  
-      def decrypt(encrypted_data)
-        iv, encrypted_message = encrypted_data.split('--').map { |part| part.unpack1('m') }
-        @cipher.decrypt
-        @cipher.key = @key
-        @cipher.iv = iv
-        @cipher.update(encrypted_message) + @cipher.final
-      end
+    def initialize
+      @key = ENV['KAFKR_KEY'] || 'default_key'
+      @cipher = OpenSSL::Cipher.new(ALGORITHM)
     end
+  
+    def encrypt(data)
+      @cipher.encrypt
+      @cipher.key = @key
+      iv = @cipher.random_iv
+
+      encrypted_data = @cipher.update(data) + @cipher.final
+      Base64.encode64(iv + encrypted_data)
+    end
+  
+    def decrypt(encrypted_data)
+      decoded_data = Base64.decode64(encrypted_data)
+      iv = decoded_data[0..15]
+      encrypted_message = decoded_data[16..-1]
+
+      @cipher.decrypt
+      @cipher.key = @key
+      @cipher.iv = iv
+
+      @cipher.update(encrypted_message) + @cipher.final
+    end
+  end
 end
