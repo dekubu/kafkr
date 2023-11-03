0e79e5874c5fd66975c113ed8dd8fe469bcc2fba
Delaney Burke <delaney@vidtreon.com>
2023-11-03 07:47:00 +0200

    removed gibberrish

--- lib/kafkr/encryptor.rb
+++ lib/kafkr/encryptor.rb
@@ -1,24 +1,31 @@
 require "openssl"
 require "base64"
-require "gibberish"
 
 module Kafkr
   class Encryptor
-    attr_reader :cipher
+    ALGORITHM = 'aes-256-cbc'
+
     def initialize
-      @cipher = Gibberish::AES.new(Base64.decode64("2wZ85yxQe0lmiQ5nsqdmPWoGB0W6HZW8S/UXVTLQ6WY="))  
+      @key = Base64.decode64("2wZ85yxQe0lmiQ5nsqdmPWoGB0W6HZW8S/UXVTLQ6WY=")
     end
 
     def encrypt(data)
-      Base64.encode64(cipher.encrypt(data))
+      cipher = OpenSSL::Cipher.new(ALGORITHM)
+      cipher.encrypt
+      cipher.key = @key
+      encrypted_data = cipher.update(data) + cipher.final
+      Base64.encode64(encrypted_data).gsub("\n", '')
     end
 
-    def decrypt(data)
-      cipher.decrypt(Base64.decode64(data))
+    def decrypt(encrypted_data)
+      decipher = OpenSSL::Cipher.new(ALGORITHM)
+      decipher.decrypt
+      decipher.key = @key
+      decrypted_data = decipher.update(Base64.decode64(encrypted_data)) + decipher.final
+      decrypted_data
     rescue OpenSSL::Cipher::CipherError => e
       puts "Decryption failed: #{e.message}"
       nil
     end
-    
   end
 end
