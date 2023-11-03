d3f53c4a54a5b3ac94784861e0021b989213b7c3
Delaney Burke <delaney@vidtreon.com>
2023-11-03 07:31:26 +0200

    should work

--- lib/kafkr/encryptor.rb
+++ lib/kafkr/encryptor.rb
@@ -3,16 +3,15 @@ require "base64"
 
 module Kafkr
   class Encryptor
-    
+    ALGORITHM = "AES-256-CBC"
+
     attr_reader :key, :cipher
 
-    ALGORITHM = 'AES-256-CBC'
-  
     def initialize
-      @key = Base64.decode64("2wZ85yxQe0lmiQ5nsqdmPWoGB0W6HZW8S/UXVTLQ6WY=") 
+      @key = Base64.decode64("2wZ85yxQe0lmiQ5nsqdmPWoGB0W6HZW8S/UXVTLQ6WY=")
       @cipher = OpenSSL::Cipher.new(ALGORITHM)
     end
-  
+
     def encrypt(data)
       cipher = OpenSSL::Cipher.new(ALGORITHM)
       cipher.encrypt
@@ -20,32 +19,28 @@ module Kafkr
       iv = cipher.random_iv
 
       encrypted_data = cipher.update(data) + cipher.final
-      Base64.encode64(iv + encrypted_data)
+      Base64.encode64(iv + encrypted_data) # Prepend the IV to the encrypted data
     end
 
     def decrypt(encrypted_data)
+      cipher = OpenSSL::Cipher.new(ALGORITHM)
+      cipher.decrypt
+      cipher.key = @key
+
       decoded_data = Base64.decode64(encrypted_data)
-    
-      # Verify the IV length is correct
-      unless decoded_data.length >= cipher.iv_len
-        puts "Invalid data: IV length is too short"
-        return nil
-      end
-    
-      cipher.iv = decoded_data[0, cipher.iv_len]
-      encrypted_message = decoded_data[cipher.iv_len..]
-    
-      # Verify the encrypted message isn't empty
-      if encrypted_message.nil? || encrypted_message.empty?
-        puts "Invalid data: Encrypted message is missing"
-        return nil
-      end
-    
+
+      # Extract the IV from the beginning of the decoded data
+      iv = decoded_data[0, cipher.iv_len]
+      cipher.iv = iv
+
+      # Extract the encrypted message, which is everything after the IV
+      encrypted_message = decoded_data[cipher.iv_len..-1]
+
+      # Decrypt and return the plaintext message
       cipher.update(encrypted_message) + cipher.final
     rescue OpenSSL::Cipher::CipherError => e
       puts "Decryption failed: #{e.message}"
       nil
     end
-    
   end
 end
