a63daee9cd554ab57ab69af31fb5543a67efaccd
Delaney Burke <delaney@vidtreon.com>
2023-11-10 21:48:33 +0200

    puts the name

--- lib/kafkr/consumer.rb
+++ lib/kafkr/consumer.rb
@@ -111,6 +111,7 @@ module Kafkr
     
       # Check if the handler is already loaded
       if $loaded_handlers.key?(handler_name)
+        p name 
         puts "#{name} loaded and is ignoring messages check the handle method"
         return
       end
