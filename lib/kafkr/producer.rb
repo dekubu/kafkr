480ac1c1363296d90d91bd9143e60c060799937a
Delaney Burke <delaney@vidtreone.com>
2023-11-18 10:09:16 +0200

    check for valid reply

--- lib/kafkr/producer.rb
+++ lib/kafkr/producer.rb
@@ -119,7 +119,8 @@ module Kafkr
     def self.send_message_and_wait(message)
       # Using method(:send_message) to pass the send_message method as a callable object
       Consumer.new.listen_for(message, self.method(:send_message)) do |received_message|
-        puts "Received message: #{received_message}"
+        ok = message["sync_uid"] == received_message["sync_uid"]
+        puts "Received message: #{received_message} is reply for message #{ok ? "OK" : "KO"}"
       end
     end
 
