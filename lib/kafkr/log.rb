85d4231aae227fc61b9c68155b261dafae56fcb6
Delaney Burke <delaney@vidtreon.com>
2024-04-11 00:51:49 +0300

    change to >> for iboun mesages

--- lib/kafkr/log.rb
+++ lib/kafkr/log.rb
@@ -76,6 +76,7 @@ module Kafkr
         message = JSON.parse(message)
         return message["uuid"], message
       rescue JSON::ParserError => e
+        puts ">> #{message}"
         match_data = /^(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}): (.+)$/.match(message)
         match_data ? [match_data[1], match_data[2]] : [nil, nil]
       end
